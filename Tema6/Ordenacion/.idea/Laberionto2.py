ceeddb6bd92a97514a23fa34db74d19a4629238f
Andrés López Zamarreño <lopez.zama@gmail.com>
2023-02-14 10:42:39 +0100

    update

--- Tema6/Ordenacion/.idea/Laberionto2.py
+++ Tema6/Ordenacion/.idea/Laberionto2.py
@@ -4,46 +4,31 @@
     [0,0,0,7],
     [0,0,0,0]
 ]'''
-'''grafo =[
+grafo =[
     [0,1,0,0,0],
     [0,0,1,6,9],
-    [0,0,0,1,2],
+    [0,0,0,1,5],
     [0,0,0,0,3],
     [0,0,0,0,0]
-]'''
-grafo =[
-    [0,0,0,4,0],
-    [0,0,0,0,0],
-    [0,3,0,0,2],
-    [0,0,2,0,1],
-    [0,4,0,0,0]
 ]
-#letras = ["A","B","C","D"]
-letras = ["A","B","C","D","E"]
 posibilidades2 = []
-final = "B"
+final = "4"
 
 def Dijkstra(laberinto):
     minimo = 100
     posicion = ""
-
     for i in range(len(laberinto)):
         for j in range(len(laberinto[i])):
-            valor = (letras[i]+letras[j]+str(laberinto[i][j]))
+            valor = (str(i)+str(j)+str(laberinto[i][j]))
             if valor[2:] != "0":
+                #print(valor)
                 posibilidades2.append(valor)
-
                 for k in range(len(posibilidades2)-1):
-                    print(posibilidades2[k])
                     if posibilidades2[k][1] == valor[0]:
                         num = int(posibilidades2[k][2]) + int(valor[2])
                         posibilidades2[len(posibilidades2)-1] =posibilidades2[len(posibilidades2)-1][:2]+str(num)
-
-                    if posibilidades2[k][0] == valor[1]:
-                        num += int(posibilidades2[k][2])
-                        posibilidades2[k] = posibilidades2[k][:2] + str(num)
-
     for i in posibilidades2:
+        print(i)
         if i[1] == final and minimo > int(i[2:]):
             minimo = int(i[2:])
             posicion = i
