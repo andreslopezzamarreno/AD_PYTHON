411143ed098aa513a55cfc07d8cbc7e589867c9e
Andrés López Zamarreño <lopez.zama@gmail.com>
2023-02-28 11:03:00 +0100

    update

--- Tema6/Ordenacion/.idea/Laberinto.py
+++ Tema6/Ordenacion/.idea/Laberinto.py
@@ -1,16 +1,16 @@
-grafo = [
+'''grafo = [
     [0,2,5,8],
     [0,0,0,1],
     [0,0,0,7],
     [0,0,0,0]
-]
-'''grafo =[
+]'''
+grafo =[
     [0,1,0,0,0],
     [0,0,1,6,9],
     [0,0,0,1,2],
     [0,0,0,0,3],
     [0,0,0,0,0]
-]'''
+]
 '''grafo =[
     [0,0,0,4,0],
     [0,0,0,0,0],
@@ -18,10 +18,10 @@ grafo = [
     [0,0,2,0,1],
     [0,4,0,0,0]
 ]'''
-letras = ["A","B","C","D"]
-#letras = ["A","B","C","D","E"]
+#letras = ["A","B","C","D"]
+letras = ["A","B","C","D","E"]
 posibilidades2 = []
-final = "D"
+final = "E"
 
 def Dijkstra(laberinto):
     minimo = 100
@@ -34,7 +34,6 @@ def Dijkstra(laberinto):
                 posibilidades2.append(valor)
 
                 for k in range(len(posibilidades2)-1):
-                    print(posibilidades2[k])
                     if posibilidades2[k][1] == valor[0]:
                         num = int(posibilidades2[k][2]) + int(valor[2])
                         posibilidades2[len(posibilidades2)-1] =posibilidades2[len(posibilidades2)-1][:2]+str(num)
@@ -47,6 +46,13 @@ def Dijkstra(laberinto):
         if i[1] == final and minimo > int(i[2:]):
             minimo = int(i[2:])
             posicion = i
+
+    '''for i in reversed(posibilidades2):
+        if posicion[:1] == i[1:2]:
+            print(posicion[1:2])
+            posicion = i
+            print(f'i vale {i}')'''
+
     return f'El camino mas corto es llegando por {posicion}'
 
 
